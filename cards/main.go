37f51a86fc9e928bd3a5fb285fa21a92b2148805
Thomas <thomas.ribeiro2392@gmail.com>
2025-01-05 12:42:12 -0300

    Fix

--- cards/main.go
+++ cards/main.go
@@ -5,7 +5,7 @@ import "fmt"
 var deckSize int
 
 func main() {
-	cards := []string{"Ace of Diamond", newCard()}
+	cards := []string{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
 	for i, card := range cards {
