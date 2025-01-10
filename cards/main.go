ef03996713db1a0bf188731a2eb4bd285a9a8238
Thomas <thomas.ribeiro2392@gmail.com>
2025-01-10 11:57:23 -0300

    Save to file function

--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,11 @@
 package main
 
-import "fmt"
-
 var deckSize int
 
 func main() {
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.saveToFile("my_deck")
+	//fmt.Println(cards.toString())
 	//hand, remainingDeck := deal(cards, 5)
 	//hand.print()
 	//remainingDeck.print()
