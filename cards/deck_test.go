45f0d110a6cf4f0f286118e928ca9479cecf5c02
Thomas <thomas.ribeiro2392@gmail.com>
2025-01-14 08:53:42 -0300

    A few more checks on test of newDeck

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -7,4 +7,10 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck lenght to be 52, but got %v", len(d))
 	}
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades as first card, but got %v", d[0])
+	}
+	if d[len(d)-1] != "King of Club" {
+		t.Errorf("Expected King of Club as last card, but got %v", d[len(d)-1])
+	}
 }
