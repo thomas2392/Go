53dde012e1f846b22d405c1a60c6c6846ef92610
Thomas <thomas.ribeiro2392@gmail.com>
2025-01-14 10:19:32 -0300

    Test to save and load deck from file

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -13,4 +16,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Club" {
 		t.Errorf("Expected King of Club as last card, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
